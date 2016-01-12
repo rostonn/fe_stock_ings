e3c2360fb6e210938059ccca8f65a47a3c280d61
joshr9 <joshruchwarger@gmail.com>
2016-01-12 11:35:29 -0700

    added Google sign in button

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-$('#google').click(function(){
+$('.btn-google').click(function(){
   window.location=('https://accounts.google.com/o/oauth2/auth?response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A8080&scope=profile&client_id=856670607753-2jriljkug9mihnqkhnmbkhpom3dn3r9b.apps.googleusercontent.com')
 })
 
