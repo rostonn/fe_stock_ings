6e79634cdc00b8e47e49c6a3587966c38106d7b0
Samuel <Linehan@Samuels-MacBook-Air.local>
2016-01-13 09:33:16 -0700

    made auth changes to main.js and started refactoring styles
    adding forms to trading page
    
    adding user data into portfolio
    
    changed get requests to heroku
    
    fixing deployment issue
    
    added user settings button
    
    working on buy and sell buttons

--- main.js
+++ main.js
@@ -1,14 +1,31 @@
-$('.btn-google').click(function(){
-  window.location=('https://accounts.google.com/o/oauth2/auth?response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A8080&scope=profile&client_id=856670607753-2jriljkug9mihnqkhnmbkhpom3dn3r9b.apps.googleusercontent.com')
+
+$.ajaxSetup({
+ xhrFields: {
+   withCredentials: true
+ }
 })
 
-var qs = querystring.parse();
-if(qs.code ){
-  // TODO: remember to change once deployed
-    $.ajax('http://localhost:3000/auth/google?' + querystring.stringify(qs), {xhrFields: {withCredentials: true}, crossDomain: true})
-  .done(function(data){
-    console.log(data)
-  }).fail(function(error){
-    console.error(error)
-  })
-}
+$('.btn-google').click(function(){
+ window.location='https://skbe.herokuapp.com/auth/google'
+ //('https://accounts.google.com/o/oauth2/auth?response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A8080&scope=profile&client_id=856670607753-2jriljkug9mihnqkhnmbkhpom3dn3r9b.apps.googleusercontent.com')
+})
+
+$('#logout').click(function(){
+ $.get('https://skbe.herokuapp.com/auth/logout')
+ .done(function(data){
+  //  console.log(data)
+   if(data === 'logged out successfully'){
+     window.location='/'
+   } else {
+     console.log('unable to logout')
+   }
+ }).fail(function(error){
+   console.error(error)
+ })
+})
+
+$.get('https://skbe.herokuapp.com/symbols/prices').done(function(data){
+ console.log(data)
+}).fail(function(error){
+ console.log(error)
+})
