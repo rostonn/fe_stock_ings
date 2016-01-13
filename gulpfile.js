6ab6bb1c91833ccff9daabab298f421170b62d5a
Samuel <Linehan@Samuels-MacBook-Air.local>
2016-01-13 09:33:16 -0700

    made auth changes to main.js and started refactoring styles

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,8 @@
 var gulp = require('gulp');
 var scsslint = require('gulp-scss-lint');
+var postcss = require('gulp-postcss');
+var sourcemaps = require('gulp-sourcemaps');
+var autoprefixer = require('autoprefixer');
 
 gulp.task('scss-lint', function() {
   return gulp.src('./scss/*.scss')
@@ -10,3 +13,11 @@ gulp.task('test', function(){
   console.log('Hello World');
   return;
 })
+
+gulp.task('autoprefixer', function () {
+  return gulp.src('./scss/*.scss')
+        .pipe(sourcemaps.init())
+        .pipe(postcss([ autoprefixer({ browsers: ['last 2 versions'] }) ]))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('./dest'));
+});
--- main.js
+++ main.js
@@ -1,14 +1,25 @@
-$('.btn-google').click(function(){
-  window.location=('https://accounts.google.com/o/oauth2/auth?response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A8080&scope=profile&client_id=856670607753-2jriljkug9mihnqkhnmbkhpom3dn3r9b.apps.googleusercontent.com')
+$.ajaxSetup({
+ xhrFields: {
+   withCredentials: true
+ }
 })
 
-var qs = querystring.parse();
-if(qs.code ){
-  // TODO: remember to change once deployed
-    $.ajax('http://localhost:3000/auth/google?' + querystring.stringify(qs), {xhrFields: {withCredentials: true}, crossDomain: true})
-  .done(function(data){
-    console.log(data)
-  }).fail(function(error){
-    console.error(error)
-  })
-}
+$('.btn-google').click(function(){
+ window.location='http://localhost:3000/auth/google'
+ //('https://accounts.google.com/o/oauth2/auth?response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A8080&scope=profile&client_id=856670607753-2jriljkug9mihnqkhnmbkhpom3dn3r9b.apps.googleusercontent.com')
+})
+
+$('.logout').click(function(){
+ $.get('http://localhost:3000/auth/logout')
+ .done(function(data){
+   console.log(data)
+ }).fail(function(error){
+   console.error(error)
+ })
+})
+
+$.get('http://localhost:3000/symbols/prices').done(function(data){
+ console.log(data)
+}).fail(function(error){
+ console.log(error)
+})
